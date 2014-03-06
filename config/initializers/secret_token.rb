0469dd7e47b34cca21f485a15c10df8b0d2acd5d
Juan Carlos <juanjcsr@gmail.com>
2014-03-06 10:38:16 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TaxiComentarios::Application.config.secret_key_base = '65d325b611ce01652b532a168caa7e3a939d21a4019f441cf0642d003e41f01f64dec695137e12f66fd4378e2a148b764beb948eaaf76cb549b861efe345aaf6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TaxiComentarios::Application.config.session_store :cookie_store, key: '_taxi-comentarios_session'
